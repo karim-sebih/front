1c4fb2d591873a48fa5c34636afc282776e5d1b6
karim-sebih <karim.sebih@laplateforme.io>
2026-03-18 12:11:20 +0100

    config

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   timeout: 10000,
-  // headers: { 'ngrok-skip-browser-warning': 'true' }  ← commente ou supprime cette ligne
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  },
 });
 instance.interceptors.request.use(
   async (config) => {
